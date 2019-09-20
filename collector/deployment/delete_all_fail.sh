574dc73864e8be9573303ceb050d4f672da44ae3
Dave Weilert <daveweilert@us.ibm.com>
2019-09-19 19:46:52 -0500

    initial load

--- collector/deployment/delete_all_fail.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete -f 02_fail_avail_one.yaml        
-kubectl delete -f 03_fail_baker_one.yaml        
-kubectl delete -f 04_fail_carbon_all.yaml        
-kubectl delete -f 05_fail_doors_all.yaml        
-kubectl delete -f 06_fail_eagle_all.yaml        
-kubectl delete -f 07_fail_floor_all.yaml
-kubectl delete -f 08_fail_gonzo_all.yaml
